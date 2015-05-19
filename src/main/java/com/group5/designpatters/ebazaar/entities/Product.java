f7ff48853f8068461da6dc0a60e1ce4d0aefe9ea
mohdrashed <abuomar_rashed@yahoo.com>
2015-05-19 11:03:20 -0500

    Classes
    Created classes for the Ebazzar project

---
+++ src/main/java/com/group5/designpatters/ebazaar/entities/Product.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.group5.designpatters.ebazaar.entities;
+
+/**
+ *
+ * @authors Moh'd Rashed Obaidat  
+ *          Odiljon Sattarov 
+ *          Rustam Ishmatov
+ */
+public class Product {
+    private long id;
+    private String description;
+    private String title;
+    private double price;
+    private int quantity;
+    private User user;
+    private Category category;
+
+    public Product() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
+    
+}
